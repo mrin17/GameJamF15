22e463eaa3ccec73214a6d63a827491c600db673
barrettand <barrett.and@husky.neu.edu>
2015-10-09 19:32:14 -0400

    "Basic Movement and Stage"

---
+++ GameJamF15/Assets/CubeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(new Vector3(-.1f, 0, 0));
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(new Vector3(.1f, 0, 0));
+		}
+	}
+}
