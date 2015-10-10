01859d7006381c2cbbd2347f6bf931b9e9ed67fd
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-10 18:45:14 -0400

    crumble animation, interpolation

--- GameJamF15/Assets/CubeControl.cs
+++ GameJamF15/Assets/CubeControl.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class CubeControl : MonoBehaviour {
 
 	float lastDirection = 1;
+	bool walking = false;
+	Animator anim;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -16,13 +18,19 @@ public class CubeControl : MonoBehaviour {
 			transform.Translate(new Vector3(-.1f, 0, 0));
 			if (!GetComponent<playerAttack>().isAttacking())
 				lastDirection = -1;
+			walking = true;
 		}
 		else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate(new Vector3(.1f, 0, 0));
 			if (!GetComponent<playerAttack>().isAttacking())
 				lastDirection = 1;
+			walking = true;
+		}
+		else {
+			walking = false;
 		}
 		transform.localScale = new Vector3 (lastDirection, 1, 1);
+		anim.SetBool ("walking", walking);
 	}
 
 	public float getLastDir() {
