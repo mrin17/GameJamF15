886e2cb0df8e7c51a28670be090f1f51993ac22b
barrettand <barrett.and@husky.neu.edu>
2015-10-09 21:11:50 -0400

    Water Pick
    IDK

--- GameJamF15/Assets/CubeControl.cs
+++ GameJamF15/Assets/CubeControl.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class CubeControl : MonoBehaviour {
-
+	public Material push;
+	public Material flip;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,11 +11,24 @@ public class CubeControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+    if (Input.GetKey (KeyCode.Z)) {
+			renderer.material = push;	
+		}
+    if (Input.GetKey (KeyCode.X)) {
+			renderer.material = flip;	
+		}
 	if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate(new Vector3(-.1f, 0, 0));
 		}
 		else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate(new Vector3(.1f, 0, 0));
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.name.Equals ("Block")) {
+		 col.gameObject.rigidbody2D.AddForce (new Vector2 (0, 5));
 		}
 	}
 }
