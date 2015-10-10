feb0ff933c78f13c80ba9e76cc0b8cfa509c2b8a
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-09 21:05:43 -0400

    bomb, bomb creation, explosion

---
+++ GameJamF15/Assets/Scripts/bomb.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Mike - this class represents a bomb
+public class bomb : MonoBehaviour {
+
+	Vector2 force = new Vector2(0, 0);
+	const float INITIAL_VELOCITY = .03f;
+
+	// Use this for initialization
+	void Start () {
+		if ((Vector2) transform.position == new Vector2(bombSpawner.SPAWN_X_LEFT, bombSpawner.SPAWN_Y))
+			force = new Vector2(INITIAL_VELOCITY, 0);
+		else if ((Vector2) transform.position == new Vector2(bombSpawner.SPAWN_X_RIGHT, bombSpawner.SPAWN_Y))
+			force = new Vector2(-INITIAL_VELOCITY, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (force);
+	}
+
+	//different types of collisions
+	//1) another bomb, they both blow up
+	//2) player's attack 1: the bomb rolls back in the opposite direction
+	//3) player's attack 2: the bomb gets tossed in the air and goes in the opposite direction, not much horizontal
+	//4) hits the beard/tower/whatever, and the player loses
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Bomb") {
+			Explode ();
+		}
+	}
+
+	void Explode() {
+		GameObject g = (GameObject) Instantiate (Resources.Load ("preExplosion"), new Vector3(transform.position.x, transform.position.y, -5), Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
---
+++ GameJamF15/Assets/Scripts/bombSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Every so often, this class spawns a bomb
+public class bombSpawner : MonoBehaviour {
+
+
+	public const float SPAWN_X_LEFT = -13;
+	public const float SPAWN_Y = -3.5f;
+	public const float SPAWN_X_RIGHT = 13;
+
+	const float BETWEEN_BOMB_MAX = .5f;
+	float betweenBombTimer = 0;
+	const int RATIO = 100;
+
+	List<GameObject> bombs = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (betweenBombTimer > 0)
+			betweenBombTimer -= Time.deltaTime;
+		else {
+			if (Random.Range(0, RATIO) == 0) {
+				CreateBomb();
+			}
+		}
+	}
+
+	void CreateBomb() {
+		Vector2 startPos;
+		if (Random.Range (0, 2) == 0)
+			startPos = new Vector2(SPAWN_X_LEFT, SPAWN_Y);
+		else
+			startPos = new Vector2(SPAWN_X_RIGHT, SPAWN_Y);
+		GameObject b = (GameObject) Instantiate (Resources.Load ("preBomb"), startPos, Quaternion.identity);
+		bombs.Add (b);
+	}
+}
