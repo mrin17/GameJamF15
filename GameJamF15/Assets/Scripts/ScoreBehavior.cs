8018db316d7fae5d603a5ad1496afbaaa0460fda
barrettand <barrett.and@husky.neu.edu>
2015-10-11 12:59:41 -0400

    Some Sounds added
    Muzak

--- GameJamF15/Assets/Scripts/ScoreBehavior.cs
+++ GameJamF15/Assets/Scripts/ScoreBehavior.cs
@@ -5,6 +5,7 @@ public class ScoreBehavior : MonoBehaviour {
     TextMesh s;
 	// Use this for initialization
 	void Start () {
+        DontDestroyOnLoad(this.gameObject);
         s = ((GameObject)Instantiate(Resources.Load("preScore"), new Vector3(-4, -4, 10), Quaternion.identity)).GetComponent<TextMesh>();
         s.text = "Score: " + FindObjectOfType<playerAttack>().getScore().ToString();
     }
